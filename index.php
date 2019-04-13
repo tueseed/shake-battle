a846f9d8ad728d3f1166c76cbd4f7ba06013894b
Nutthapong <tue_seed@hotmail.com>
2019-04-13 23:06:03 +0700

    sdasdfas

--- index.php
+++ index.php
@@ -16,13 +16,13 @@
                 <div class="col-lg-10 text-center">
                     <h1>Shake Battle </h1>
                     <p>
-                        <div class="progress bg-dark" style="hight:50px">
+                        <div class="progress bg-dark">
                             <div class="progress-bar bg-info progress-bar-striped" id="progressbar" style="width:80%"></div>
                         </div>
                     </p> 
                 </div>
                 <div class="col-lg-2 text-center mt-2">
-                    <img src=""  class="img-profile rounded-circle" id="image" width="150" height="100">
+                    <img src=""  class="img-profile rounded-circle" id="image" width="50" height="50">
                     <p id="player_name"></p> 
                 </div>
             </div>
