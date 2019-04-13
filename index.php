5e7f5beaec30c56185e5ac0d14455b65309125ad
Nutthapong <tue_seed@hotmail.com>
2019-04-13 12:22:45 +0700

    sadsd

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
         <script src="scripts/lobby.js"></script>    
     </head>
     <body class="bg-dark">
