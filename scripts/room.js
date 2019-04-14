66d7f724ccf9981709cb3ea5643583d9297eed5a
Nutthapong <tue_seed@hotmail.com>
2019-04-14 15:53:15 +0700

    asSAsA

--- scripts/room.js
+++ scripts/room.js
@@ -16,7 +16,7 @@ console.log('room_id' + room_id);
 var room_ref = firebase.database().ref('room/'+ room_id); 
 room_ref.on('value',function(snapshot){
     var player_inroom = snapshot.val();
-    console.log(player_inroom[0]);
+    console.log(player_inroom);
     /*var i = 0;
     while(Object.keys(player_inroom)[i])
     {
