07bef2b6c06ef1f962aaa89f28627824e018adb9
Nutthapong <tue_seed@hotmail.com>
2019-04-14 14:35:14 +0700

    room.js

--- scripts/lobby.js
+++ scripts/lobby.js
@@ -78,9 +78,8 @@ function creat_room()
     var room_key =  room.push();
 
     var player = firebase.database().ref('room/'+ room_key.key); 
-    var player_key = player.push({'playername':'tue','score':'0'});
-    console.log('room : ' + room_key.key + '  player' + player_key.key);
-    window.location.href= "room.php?room=" + room_key.key ;
+    var player_key = player.push({'playername':sessionStorage.getItem('player_name'),'score':'0'});
+    window.location.href= "room.php?room_id=" + room_key.key + "&cmd=cr" ;
 }
 function render_card(room_id,room_num)
 {
--- scripts/room.js
+++ scripts/room.js
@@ -1,4 +1,31 @@
-var room_id = getUrlVars()["room_id"];
+//// เซตรูปภาพกับชื่อ
+document.getElementById('image').setAttribute('src',sessionStorage.getItem('image'));
+document.getElementById('player_name').innerHTML = sessionStorage.getItem('player_name');
+var config = {
+    apiKey: 'AIzaSyCvnyqJxQ_oEZmESo7JFvYA5OnVLtZIHzE',
+    authDomain: 'shake-battle.firebaseapp.com',
+    databaseURL: 'https://shake-battle.firebaseio.com',
+    projectId: 'shake-battle',
+};
+firebase.initializeApp(config);
+
+var room_id = getUrlVars()["room_id"];//รับค่า room_id จาก url ที่ส่งมาจากหน้า lobby
+var cmd = getUrlVars()["cmd"];//รับค่า cmd จาก url ที่ส่งมาจากหน้า lobby
+
+var room_ref = firebase.database().ref('room/'+ room_id); 
+room_ref.on('value',function(snapshot){
+    var player_inroom = snapshot.val();
+    var i = 0;
+    while(Object.keys(player_inroom)[i])
+    {
+        cunsole.log(Object.keys(player_inroom)[i]);
+        i++;
+    }
+});
+
+
+
+
 function getUrlVars() {
     var vars = {};
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
