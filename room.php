cac6166f7165a9e3d8e69a8e3aafcdf2c6cabf67
Nutthapong <tue_seed@hotmail.com>
2019-04-14 16:05:20 +0700

    l,';'

--- room.php
+++ room.php
@@ -7,8 +7,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        <script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
-        <script src="scripts/room.js"></script>    
+        <script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>    
     </head>
     <body class="bg-dark">
         <div class="container-fluid bg-light">
@@ -32,5 +31,6 @@
                 <!--พื้นที่ห้อง-->
             </div>
         </div>
+        <script src="scripts/room.js"></script>
     </body>
 </html>
