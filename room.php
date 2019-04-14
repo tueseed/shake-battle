5b1cbfc91261033c27353a7b1b426d3386883d11
Nutthapong <tue_seed@hotmail.com>
2019-04-14 19:24:45 +0700

    aDAdD

--- room.php
+++ room.php
@@ -31,15 +31,17 @@
                 <div class="col-lg-12 text-center">
                     <div class="card">
                         <div class="cars-body">
-                            <div class="col-lg-1 text-center">
-                                <img src="https://profile.line-scdn.net/0h3CGaBdFLbGx6GEEYmtETO0ZdYgENNmokAnl2Xg8fZ19TIH5vQytzDA0aZwlWLSMzQyohC1cdNQ4E/preview" class="img-profile rounded-circle" id="image" width="50" height="50">
-                                <p>dsfsdvds</p>
-                            </div>
-                            <div class="col-lg-11 text-center">
-                            <div class="progress bg-dark">
-                                <div class="progress-bar bg-info progress-bar-striped" id="progressbar" style="width:80%"></div>
-                            </div>
-                            </div>
+                            <div class="row">                        
+                                <div class="col-lg-1 text-center">
+                                    <img src="https://profile.line-scdn.net/0h3CGaBdFLbGx6GEEYmtETO0ZdYgENNmokAnl2Xg8fZ19TIH5vQytzDA0aZwlWLSMzQyohC1cdNQ4E/preview" class="img-profile rounded-circle" id="image" width="50" height="50">
+                                    <p>dsfsdvds</p>
+                                </div>
+                                <div class="col-lg-11 text-center">
+                                    <div class="progress bg-dark">
+                                        <div class="progress-bar bg-info progress-bar-striped" id="progressbar" style="width:80%"></div>
+                                    </div>
+                                </div>
+                            </div>    
                         </div>
                     </div>
                 </div>
