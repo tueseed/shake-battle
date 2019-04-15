47a63831508533fdbedab78f49dd94e73afbc0d3
Nutthapong <tue_seed@hotmail.com>
2019-04-15 20:32:17 +0700

    fsefsef

--- scripts/fn_test.js
+++ scripts/fn_test.js
@@ -1,4 +1,14 @@
+var config = {
+    apiKey: 'AIzaSyCvnyqJxQ_oEZmESo7JFvYA5OnVLtZIHzE',
+    authDomain: 'shake-battle.firebaseapp.com',
+    databaseURL: 'https://shake-battle.firebaseio.com',
+    projectId: 'shake-battle',
+};
+firebase.initializeApp(config);
 function query_data()
 {
-    alert("asdadsd");
+    var playersRef = firebase.database().ref("room/");
+    playersRef.orderByChild("Nutthapong").equalTo("Nutthapong").on("child_added", function(data) {
+        console.log("Equal to filter: " + data.val().playername);
+     });
 }
