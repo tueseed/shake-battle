5e0f6d0bca54767c928ef234881899164ea4d76f
Nutthapong <tue_seed@hotmail.com>
2019-04-15 20:21:42 +0700

    jk;m;kp[

---
+++ testfunction.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Shake Battle</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
+        <script src="scripts/fn_test.js"></script>
+</head>
+<body>
+    <input class="btn btn-success" value="test" onclick="query_data()">
+</body>
+</html>
