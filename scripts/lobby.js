edff34d5dfd1dc480b9f864d1b207a6a1b2521f0
Nutthapong <tue_seed@hotmail.com>
2019-04-15 20:01:31 +0700

    p'lp[l

--- scripts/lobby.js
+++ scripts/lobby.js
@@ -33,7 +33,7 @@ else
                                           var id_token = response.id_token;
                                           var base64 = id_token.split('.')[1];
                                           var profile = JSON.parse(window.atob(base64));
-                                          console.log(profile.name);
+                                          console.log(profile);
                                           console.log(profile.picture);
                                           document.getElementById('image').setAttribute('src',profile.picture);
                                           document.getElementById('player_name').innerHTML = profile.name;
