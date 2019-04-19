5b30daa9f3bd1ab2e0d297f680885d276b15a555
Nutthapong <tue_seed@hotmail.com>
2019-04-19 19:14:38 +0700

    กหดหกหด

--- scripts/fn_test.js
+++ scripts/fn_test.js
@@ -5,11 +5,11 @@ var config = {
     projectId: 'shake-battle',
 };
 firebase.initializeApp(config);
-var playersRef = firebase.database().ref("room").child().child("player");
+var playersRef = firebase.database().ref("room").child("player");
 
 function query_data()
 {
-    playersRef.orderByChild("playername").equalTo("Nutthapong").then("value", function(data) {
+    playersRef.orderByChild("playername").equalTo("Nutthapong").then("child_added", function(data) {
         /*if(data == "")
         {
             alert("null เว้ยยยยย......");
